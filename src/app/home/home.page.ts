97bda4b7f1e437f26a08abf89270751bd5c9b7e8
Annie Perez <116457128+shellperez@users.noreply.github.com>
2023-01-12 23:48:21 -0500

    actividad1 12/01/23

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  slideOpt ={
+    initialSlide: 0, //slide inicial (primero) [0,1,2,3]
+    slidePerView: 1, //configuramos un slide por vista
+    centerSlides: true,//
+    speed: 100 //velocidad movimiento de los slides
+  }
 
   constructor() {}
 
